5ff2e78f1ce6d9d4081d0aab4a941734664fbb97
KrutikaChincholkar07 <chincholkarkrutika07@gmail.com>
2025-09-11 22:51:38 +0530

    Create TrianglePatternR.java
    Right-angled triangle star pattern aligned to the right code in Java
    Output:
          *
        * *
      * * *
    * * * *

---
+++ TrianglePatternR.java
@@ -0,0 +1,17 @@
+public class RightAlignedTriangle {
+    public static void main(String[] args) {
+        int rows = 4;
+
+        for (int i = 1; i <= rows; i++) {
+            // Print spaces
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("  ");
+            }
+            // Print stars
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
